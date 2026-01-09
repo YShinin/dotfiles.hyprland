39d95096488ebfc4141e540178d6de83116bed35
Yehor <deacon.core@gmail.com>
2026-01-09 13:06:04 +0200

    wallpaper-changer.sh development

--- .config/wallpaper/wallpaper-changer.sh
+++ .config/wallpaper/wallpaper-changer.sh
@@ -34,11 +34,15 @@ default_hc="$HOME/.config/hypr/modules/current-wallpaper.conf"
 default_kps="$HOME/.config/wallust/kitty-correction.py"
 default_kitty_colors_raw="$HOME/.config/kitty/colors-raw.conf"
 default_kitty_colors_tweaked="$HOME/.config/kitty/colors-tweaked.conf"
+default_rasi_file="$HOME/.config/rofi/wallpaper-changer.rasi"
 
+wallpaper_directory=$default_wd
+cache_directory=$default_cd
 wallpaper_hypr_config=$default_hc
 kitty_python_script=$default_kps
 kitty_colors_raw=$default_kitty_colors_raw
 kitty_colors_tweaked=$default_kitty_colors_tweaked
+rasi_file=$default_rasi_file
 
 while [[ -n "$1" ]]; do
   case "$1" in
@@ -54,6 +58,18 @@ while [[ -n "$1" ]]; do
       shift
       wallpaper_hypr_config="$1"
       ;;
+    -krc | --kitty-raw-colors)
+      shift
+      kitty_colors_raw="$1"
+      ;;
+    -ktc | --kitty-tweaked-colors)
+      shift
+      kitty_colors_tweaked="$1"
+      ;;
+    -rf | --rasi-file)
+      shift
+      rasi_file="$1"
+      ;;
     -h | --help)
       echo "Usage: $(basename "$0") [args]
       -h  | --help    Show this menu
@@ -64,9 +80,18 @@ while [[ -n "$1" ]]; do
       -cd | --cache-directory    Pass there your path to cache directory used for keeping image previews
           (default = ${default_cd})
           
+      -rf | --rasi-file    Pass there path to rasi file for rofi
+          (default = ${default_rasi_file})
+          
       -hc | --hyprland-config    Pass there your path to config file where current wallpaper will be stored
         [NOTE file will be fully cleared after wallpaper change]
-          (default = ${default_hc})"
+          (default = ${default_hc})
+          
+      -krc | --kitty-raw-colors    Pass there path to file with wallust colors generated with for kitty
+          (default = ${default_kitty_colors_raw})
+          
+      -ktc | --kitty-tweaked-colors    Pass there path to file that will be imported to kitty (file must be separate from kitty-raw-colros)
+          (default = ${default_kitty_colors_tweaked})"
       exit
       ;;
     *)
@@ -77,54 +102,50 @@ while [[ -n "$1" ]]; do
   shift
 done
 
-if [[ -z $wallpaper_directory ]]; then
-  wallpaper_directory=${default_wd}
-fi
-
 if [[ ! (-d $wallpaper_directory) ]]; then
   notify-send -t 3000 "ERROR: Wallpaper changer" "${wallpaper_directory} doesn't exist" 
+  exit 2
+fi
+
+if [[ ! -f $kitty_colors_raw ]]; then
+  notify-send -t 3000 "ERROR: Wallpaper changer" "${kitty_colors_raw} doesn't exist" 
   exit 3
 fi
 
-# ^ ANCHER TO THE START OF LINE
-# [^.]+ ANY CHARACTER EXCEPT DOT ONE OR MORE TIME
-# \. SECURED DOT
-# (...)$ ONE OF THE EXTENTIONS AT THE END
-ls_result=$(ls "$wallpaper_directory" | grep -E "^[^.]+\.(png|jpg|webm|jpeg)$")
-
-
-if [[ -z "$ls_result" ]]; then
-  notify-send -t 3000 "ERROR: Wallpaper changer" "$wallpaper_directory has no image files"
-  exit 4
+if [[ ! -f $kitty_colors_tweaked ]]; then
+  notify-send -t 3000 "ERROR: Wallpaper changer" "${kitty_colors_tweaked} doesn't exist" 
+  exit 3
 fi
 
-# echo "wd - $wallpaper_directory    ls result - $ls_result   ls result len ${#ls_result} "
-
-if [[ -z "$cache_directory" ]]; then
-  cache_directory=${default_cd}
-fi
+shopt -s nullglob
+shopt -s nocaseglob
 
 if [[ -e "$cache_directory" ]]; then
   if [[ ! ( -d "$cache_directory" ) ]]; then
     notify-send -t 3000 "ERROR: Wallpaper changer" "$cache_directory exists and it is not a directory"
-    exit 5
+    exit 2
   fi
 else
   mkdir -p "$cache_directory"
 fi
 
-chosen_image=$( for image_without_path in $ls_result
-do
-  if [[ ! ( $image_without_path =~ \.(png|jpg|webm|jpeg)$ ) ]]; then
-    notify-send -t 3000 "ERROR: Wallpaper changer" "File can't contain white space!"
-  fi
+cd "$wallpaper_directory" || { notify-send "ERROR: Wallpaper changer" "cd ${wallpaper_directory} failed"; exit 1; }
 
-  image_without_extention=$(basename -s .* "$image_without_path")
-  if [[ ! ( -f "${cache_directory}${image_without_path}")]]; then
-    magick "${wallpaper_directory}${image_without_path}" -resize 300x200 "${cache_directory}${image_without_path}"
-  fi
-  echo -en "${image_without_extention}\0icon\x1f${cache_directory}${image_without_path}\n"
-done | rofi -show-icons -dmenu -no-config )
+chosen_image=$(
+  for image_file in *.{png,jpg,jpeg}; do
+
+    [[ -e "$image_file" ]] || continue
+
+    image_without_extention="${image_file%.*}"
+    notify-send "image file is ${image_file}" "image_without_extention ${image_without_extention}"
+    
+    target_cache_file="${cache_directory%/}/${image_file}"
+    
+    if [[ ! -f $target_cache_file ]]; then
+      magick "${image_file}" -resize 300x200 "${target_cache_file}"
+    fi
+    echo -en "${image_without_extention}\0icon\x1f${target_cache_file}\n"
+done | rofi -show-icons -dmenu -config ${rasi_file})
 
 [[ ! -z $chosen_image ]] || exit 0
 
@@ -133,20 +154,29 @@ if [[ ! -f $wallpaper_hypr_config ]]; then
 
   if [[ ! $? ]]; then
     notify-send -t 3000 "ERROR: Wallpaper changer" "Coudn't create config file in {$wallpaper_hypr_config}, verify if directory is persent"
-    exit 6
+    exit 2
   fi
 fi
 
-chosen_image=$(echo $ls_result | grep -o $chosen_image)
+found_file=$(find . -maxdepth 1 -name "${chosen_image}.*" -print -quit)
+
+chosen_image=$(basename "$found_file")
+
+if [[ -z "$chosen_image" ]]; then
+    notify-send "ERROR: Wallpaper changer" "Could not find image file for selection"
+    exit 1
+fi
+
+chosen_image="${wallpaper_directory%/}/${chosen_image}"
 
 wallpaper_in_config=$(grep "^\$wallpaper_in_config" "$wallpaper_hypr_config" | sed "s/.*= //" | sed 's/#.*//')
 
-if [[ -z $wallpaper_in_config || $wallpaper_in_config != ${wallpaper_directory}${chosen_image} ]]; then
-  echo "\$wallpaper_in_config = ${wallpaper_directory}${chosen_image}" > ${wallpaper_hypr_config}
+if [[ -z $wallpaper_in_config || $wallpaper_in_config != ${chosen_image} ]]; then
+  echo "\$wallpaper_in_config = ${chosen_image}" > ${wallpaper_hypr_config}
   
   if [[ ! $? ]]; then
     notify-send -t 3000 "ERROR: Wallpaper changer" "Couldn't write new wallpaper path in ${wallpaper_hypr_config}"
-    exit 7
+    exit 3
   fi
   
   swww img $chosen_image
